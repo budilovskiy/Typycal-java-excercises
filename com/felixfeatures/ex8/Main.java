5765dfe9d5a482cbba12a06c7533ccbdb7b9a499
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-11-01 13:59:41 +0200

    Update Main.java

--- com/felixfeatures/ex8/Main.java
+++ com/felixfeatures/ex8/Main.java
@@ -1,9 +1,5 @@
 package com.felixfeatures.ex8;
 
-import java.util.Arrays;
-import java.util.function.IntConsumer;
-import java.util.function.IntUnaryOperator;
-
 /**
  * 8. Последовательность действительных чисел оканчивается нулем.
  * Найти количество членов этой последовательности.
