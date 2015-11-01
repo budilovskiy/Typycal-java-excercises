31928719bbea93f2a4df453acb57e195f96f57d3
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-11-01 19:12:46 +0200

    Create Main.java

---
+++ com/felixfeatures/ex030/Main.java
@@ -0,0 +1,49 @@
+package com.felixfeatures.ex030;
+
+import java.util.Arrays;
+
+/**
+ * 30. Дана последовательность целых чисел a1, a2, ..., аn.
+ * Образовать новую последовательность, выбросив из исходной те члены, которые равны  min(a1, a2, ..., аn).
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {10, 11, 10000, 12, 20, 100, 10, 1000, 18};
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(removeMins(array)));
+    }
+
+    public static int[] removeMins(int[] array) {
+        int counter = 0;
+        int min = array[0];
+
+        // find min element
+        for (int element : array) {
+            if (element < min) {
+                min = element;
+            }
+        }
+
+        // count min elements
+        for (int element : array) {
+            if (element == min) {
+                counter++;
+            }
+        }
+
+        // create new array
+        int[] result = new int[array.length - counter];
+
+        // filling result array
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != min) {
+                result[j++] = array[i];
+            }
+        }
+
+        return result;
+    }
+
+}
