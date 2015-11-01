ab0d6ddffb24b22b4bbf7beaf35837db45423a4b
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-11-01 15:14:58 +0200

    Create Main.java

---
+++ com/felixfeatures/ex014/Main.java
@@ -0,0 +1,24 @@
+package com.felixfeatures.ex014;
+
+/**
+ * 14. При поступлении в вуз абитуриенты, получившие "двойку" на первом экзамене, ко второму не допускаются.
+ * В массиве A[n] записаны оценки экзаменующихся, полученные на первом экзамене.
+ * Подсчитать, сколько человек не допущено ко второму экзамену.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {5, 5, 4, 2, 3, 5, 2, 3, 3, 4, 2};
+        System.out.println(countMarks(array));
+    }
+
+    public static long countMarks(int[] array) {
+        long counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 2) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
