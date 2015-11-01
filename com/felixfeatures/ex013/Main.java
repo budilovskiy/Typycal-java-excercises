037440019102ea0edd0b6c2d9c97b4571e6715c6
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-11-01 15:09:11 +0200

    Update and rename com/felixfeatures/ex012/Main.java to com/felixfeatures/ex013/Main.java

---
+++ com/felixfeatures/ex013/Main.java
@@ -0,0 +1,25 @@
+package com.felixfeatures.ex013;
+
+import java.util.Arrays;
+
+/**
+ * 12. В заданном одномерном массиве поменять местами соседние элементы, стоящие на четных местах, 
+ * с элементами, стоящими на нечетных.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Object[] array = {"zero", "one", "two", "three", "four", "five"};
+        System.out.println(Arrays.toString(flipOddEvenIndexes(array)));
+    }
+
+    public static Object[] flipOddEvenIndexes(Object[] array) {
+        Object temp;
+        for (int i = 1; i < array.length; i = i + 2) {
+            temp = array[i - 1];
+            array[i - 1] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
+}
