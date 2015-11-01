1eb233fa6ce9d8b71c5c09a04d2f5c7be5360abc
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-11-01 15:18:56 +0200

    Update Main.java

--- com/felixfeatures/ex014/Main.java
+++ com/felixfeatures/ex014/Main.java
@@ -8,14 +8,14 @@ package com.felixfeatures.ex014;
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = {5, 5, 4, 2, 3, 5, 2, 3, 3, 4, 2};
-        System.out.println(countMarks(array));
+        int[] marks = {5, 5, 4, 2, 3, 5, 2, 3, 3, 4, 2};
+        System.out.println(countMarks(marks));
     }
 
-    public static long countMarks(int[] array) {
+    public static long countMarks(int[] marks) {
         long counter = 0;
-        for (int i = 0; i < array.length; i++) {
-            counter += (array[i] > 2) ? 0 : 1;
+        for (int mark : marks) {
+            counter += (mark > 2) ? 0 : 1;
         }
         return counter;
     }
